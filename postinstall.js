a4a675efb69bae28931309472e8bfc6372321668
Adrian Burlacu <adrian.burlacu@live.com>
2021-10-01 16:23:24 +0300

    Derp wrong dep.

--- postinstall.js
+++ postinstall.js
@@ -20,7 +20,7 @@ async function Main() {
     stdout = await execShellCommand(`cd ./istanbuljs/${folder} && npm i && cd ../..`);
 	  console.log(stdout);
   }
-  await execShellCommand(`cd ./istanbuljs/istanbul-lib-hook/node_modules/append-transform && npm i && cd ../../../..`);
+  await execShellCommand(`cd ./istanbuljs/istanbul-lib-hook/node_modules/default-require-extensions && npm i && cd ../../../..`);
   console.log(`2. Installed: Istanbul coverage library.`);
 }
 Main();
