b910f66942095f15388dab4351f9ac8dc908a0ff
Adrian Burlacu <adrian.burlacu@live.com>
2021-10-03 11:32:34 +0300

    Simplified SUT differences.

--- test/regular.test.js
+++ test/regular.test.js
@@ -7,10 +7,10 @@ const { fastFuzz } = require('../dist/index.js');
 describe('Regular fuzzing.', function () {
   this.timeout(0);
 
-  it('Finds solution in reasonable time.', () => {
+  it('Finds solution in reasonable time 1.', () => {
     let result = fastFuzz(
       './test/sut/regular.js',
-      'regular',
+      'regular1',
       [
         '{"type":"bool"}',
         '{"type":"int","min":0,"max":16}',
@@ -28,5 +28,28 @@ describe('Regular fuzzing.', function () {
 
     assert.ok(result);
     assert.ok(result.tests.length == 12);
+  });
+  
+  it('Finds solution in reasonable time 2.', () => {
+    let result = fastFuzz(
+      './test/sut/regular.js',
+      'regular2',
+      [
+        '{"type":"bool"}',
+        '{"type":"int","min":0,"max":16}',
+        '{"type":"int","min":0}',
+        '{"type":"int","min":0}',
+        '{"type":"bool"}',
+        '{"type":"int","min":0}'
+      ],
+      [69366, 42808, 5, 26],
+      1e4,
+      5e7,
+      true,
+      false
+    );
+
+    assert.ok(result);
+    assert.ok(result.tests.length == 7);
   });
 })
--- test/sut/regular.js
+++ test/sut/regular.js
@@ -1,7 +1,7 @@
 const constants = [69366, 42808];
 
 //                                bool     int,0,16  int,0   int,0     bool     int,0
-const regularInternal = function (isFalse, cityCode, amount, interest, isExtra, extraInterest) {
+const regularInternal1 = function (isFalse, cityCode, amount, interest, isExtra, extraInterest) {
 
   let result;
   if (cityCode == 12)
@@ -48,6 +48,63 @@ const regularInternal = function (isFalse, cityCode, amount, interest, isExtra, 
   return result;
 };
 
-module.exports.regular = function (args) {
-  return regularInternal(...args);
+//                                 bool     int,0,16  int,0   int,0     bool     int,0
+const regularInternal2 = function (isFalse, cityCode, amount, interest, isExtra, extraInterest) {
+
+  let result;
+  if (isFalse || amount === 0)
+  {
+    result = {
+      type: 'false',
+      cityCode: cityCode,
+      amount: 0
+    };
+  }
+  else if (cityCode == 12)
+  {
+    let netInterest = interest + extraInterest;
+    let extraInterestTO;
+
+    if (isExtra) {
+      if (netInterest > constants[0]) {
+        amount = 0;
+        extraInterestTO = 0;
+      } else {
+        extraInterestTO = extraInterest;  
+      }
+    } else {
+      if (netInterest > constants[1]) {
+        amount = 0;
+        extraInterestTO = 0;
+      } else {
+        extraInterestTO = 0;
+      }
+    }
+
+    result = {
+      type: 'toronto',
+      cityCode: cityCode,
+      amount: amount,
+      extraInterestTO: extraInterestTO
+    };
+  }
+  else
+  {
+    result = {
+      type: 'regular',
+      cityCode: cityCode,
+      amount: amount
+    };
+  }
+
+  return result;
+};
+
+module.exports.regular1 = function (args) {
+  return regularInternal1(...args);
+};
+
+
+module.exports.regular2 = function (args) {
+  return regularInternal2(...args);
 };
