0c59aaf1a3e4932733a6b6986efe688bbcc81b3f
Adrian Burlacu <adrian.burlacu@live.com>
2022-05-03 17:42:15 +0300

    Wait while publishing.

--- publish.js
+++ publish.js
@@ -1,11 +1,23 @@
 const execShellCommand = require("./execShellCommand.js");
+const FlatPromise = require('flat-promise');
+
 const packName = 'fast-fuzz';
+const WAIT_TIME = 2000;
 
 async function publish() {
   const latestVersion = await import('latest-version');
   let version = await latestVersion.default(packName);
   
   console.log(await execShellCommand('npm publish'));
+  await wait(WAIT_TIME);
   console.log(await execShellCommand(`npm unpublish ${packName}@${version}`));
 }
+
+async function wait(ms) {
+  let flatPromise = new FlatPromise();
+
+  setTimeout(() => flatPromise.resolve(), ms);
+  return flatPromise.promise;
+}
+
 publish();
