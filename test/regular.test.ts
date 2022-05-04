8f05cbcec7e46e959037560fd6d4208ac8eca473
Adrian Burlacu <adrian.burlacu@live.com>
2022-05-04 15:13:44 +0300

    Use a global and run the analyzer once.

--- test/regular.test.ts
+++ test/regular.test.ts
@@ -1,14 +1,17 @@
 const execShellCommand = require("../execShellCommand.js");
 import { assert } from 'chai';
 
-
 describe('Regular fuzzing.', function () {
   this.timeout(6e4);
 
-  it('Finds solution in reasonable time 1.', async () => {
+  before(async () => {
+    global.fastFuzzResults = await execShellCommand(`node ./dist/src/bin.js -i "./test/sut" -s "./" -d "../../dist/test/sut" -c "SUT_1" -q true -t 3e3`);
+  });
 
-    console.log(await execShellCommand(`node ./dist/src/bin.js -i "./test/sut" -s "./" -d "../../dist/test/sut" -c "SUT_1" -q true`));
+  it('Finds solution in reasonable time 1.', async () => {
+    console.log(global.fastFuzzResults);
 
     assert.ok(true);
   });
+
 });
